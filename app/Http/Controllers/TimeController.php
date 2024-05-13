4fc14baffdbd12fc018f6cbb5316b6bbd9f3d209
TiToso00 <rique0022@hotmail.com>
2024-05-13 15:00:12 -0300

    Update TimeController.php

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -2,83 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Time;
 use Illuminate\Http\Request;
 
 class TimeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $times = Time::all();
+        return view('times.index', compact('times'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function show(Time $time)
+    {
+        return view('times.show', compact('time'));
+    }
+
     public function create()
     {
-        //
+        return view('times.form');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        // Validação dos dados
+        $request->validate([
+            'nome' => 'required|string',
+            'cidade' => 'required|string',
+            'ano_fundacao' => 'required|integer|min:1900|max:' . date('Y'),
+        ]);
+
+        // Criação do novo time
+        Time::create($request->all());
+
+        // Redirecionamento com mensagem de sucesso
+        return redirect()->route('times.index')->with('success', 'Time criado com sucesso!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function edit(Time $time)
     {
-        //
+        return view('times.form', compact('time'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function update(Request $request, Time $time)
     {
-        //
+        // Validação dos dados
+        $request->validate([
+            'nome' => 'required|string',
+            'cidade' => 'required|string',
+            'ano_fundacao' => 'required|integer|min:1900|max:' . date('Y'),
+        ]);
+
+        // Atualização do time
+        $time->update($request->all());
+
+        // Redirecionamento com mensagem de sucesso
+        return redirect()->route('times.index')->with('success', 'Time atualizado com sucesso!');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function destroy(Time $time)
     {
-        //
-    }
+        // Exclusão do time
+        $time->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        // Redirecionamento com mensagem de sucesso
+        return redirect()->route('times.index')->with('success', 'Time excluído com sucesso!');
     }
 }
